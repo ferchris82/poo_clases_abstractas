1ff1cb7c70c03d4608223b57ebb3dc11d3f2b329
Christian <ferchris82@hotmail.com>
2023-12-12 17:20:35 -0500

    Versión 0.1

--- src/org/chrisferdev/pooclasesbstractas/form/EjemploForm.java
+++ src/org/chrisferdev/pooclasesbstractas/form/EjemploForm.java
@@ -6,6 +6,10 @@ import org.chrisferdev.pooclasesbstractas.form.elementos.SelectForm;
 import org.chrisferdev.pooclasesbstractas.form.elementos.TextAreaForm;
 import org.chrisferdev.pooclasesbstractas.form.elementos.select.Opcion;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class EjemploForm {
     public static void main(String[] args) {
 
@@ -18,16 +22,29 @@ public class EjemploForm {
 
         SelectForm lenguaje = new SelectForm("lenguaje");
         Opcion java = new Opcion("1", "Java");
-        lenguaje.addOpcion(java);
-        lenguaje.addOpcion(new Opcion("2", "Python"));
-        lenguaje.addOpcion(new Opcion("3", "JavaScript"));
-        lenguaje.addOpcion(new Opcion("4", "TypeScript"));
-        lenguaje.addOpcion(new Opcion("5", "PHP"));
+        lenguaje.addOpcion(java)
+                .addOpcion(new Opcion("2", "Python"))
+                .addOpcion(new Opcion("3", "JavaScript"))
+                .addOpcion(new Opcion("4", "TypeScript"))
+                .addOpcion(new Opcion("5", "PHP"));
 
         username.setValor("john.doe");
         password.setValor("123");
         email.setValor("john.doe@correo.com");
         edad.setValor("28");
+        experiencia.setValor("... más de 10 años de experiencia ...");
         java.setSelected(true);
+
+        List<ElementoForm> elementos = Arrays.asList(username,
+                password,
+                email,
+                edad,
+                experiencia,
+                lenguaje);
+
+        elementos.forEach(e -> {
+            System.out.println(e.dibujarHtml());
+            System.out.println("<br>");
+        });
     }
 }
